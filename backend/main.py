4315213b7d5fdb8e58eefe4646154f7fa857c41f
edouard <edouardzamo@gmail.com>
2026-06-09 13:26:13 +0100

    Update CORS settings

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rentora-frontend-three.vercel.app",
+        "https://rentora-fg8s.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
